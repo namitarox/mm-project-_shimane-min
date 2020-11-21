cc842b2758252fea32aaec8dac6720fbff7bd0ba
daichi_sato <daichiexperience@gmail.com>
2020-11-21 16:17:12 +0900

    [Fix] refs #1
    jQueryの設定、テキストの修正

---
+++ wp-content/base/js/index.js
@@ -0,0 +1,27 @@
+$(function () {
+  // var bottom_margin = 20,
+  //   $footer = $(".l-footer"),
+  //   $page_top = $(".js-page-top");
+  // $page_top.hide();
+  // $(window).scroll(function () {
+  //   if ($(this).scrollTop() > 100) {
+  //     $page_top.fadeIn();
+  //   } else {
+  //     $page_top.fadeOut();
+  //   }
+  //   var footer_start = $footer.offset().top,
+  //     scroll_bottom = $(this).scrollTop() + $(window).height();
+  //   if (scroll_bottom > footer_start) {
+  //     $page_top.css(
+  //       "bottom",
+  //       scroll_bottom - footer_start + bottom_margin + "px"
+  //     );
+  //   } else {
+  //     $page_top.css("bottom", "20px");
+  //   }
+  // });
+  // $page_top.click(function () {
+  //   $("body, html").animate({ scrollTop: 0 }, 500);
+  //   return false;
+  // });
+});
