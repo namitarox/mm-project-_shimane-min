777dd5ab0260c83523cbd746596b36686fccd39a
daichi_sato <daichiexperience@gmail.com>
2020-11-22 23:21:26 +0900

    [FIx] refs #1
    スクロールアニメーションの実装

--- wp-content/base/js/index.js
+++ wp-content/base/js/index.js
@@ -1,27 +1,39 @@
 $(function () {
-  // var bottom_margin = 20,
-  //   $footer = $(".l-footer"),
-  //   $page_top = $(".js-page-top");
-  // $page_top.hide();
-  // $(window).scroll(function () {
-  //   if ($(this).scrollTop() > 100) {
-  //     $page_top.fadeIn();
-  //   } else {
-  //     $page_top.fadeOut();
-  //   }
-  //   var footer_start = $footer.offset().top,
-  //     scroll_bottom = $(this).scrollTop() + $(window).height();
-  //   if (scroll_bottom > footer_start) {
-  //     $page_top.css(
-  //       "bottom",
-  //       scroll_bottom - footer_start + bottom_margin + "px"
-  //     );
-  //   } else {
-  //     $page_top.css("bottom", "20px");
-  //   }
-  // });
-  // $page_top.click(function () {
-  //   $("body, html").animate({ scrollTop: 0 }, 500);
-  //   return false;
-  // });
+  const scrollTop = function () {
+    const bottomMargin = 20,
+      $footer = $(".l-footer"),
+      $pageTop = $(".js-page-top");
+
+    $pageTop.hide();
+
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        $pageTop.fadeIn();
+      } else {
+        $pageTop.fadeOut();
+      }
+
+      const footerStart = $footer.offset().top,
+        scrollBottom = $(this).scrollTop() + $(window).height();
+
+      if (scrollBottom > footerStart) {
+        $pageTop.css(
+          "bottom",
+          scrollBottom - footerStart + bottomMargin + "px"
+        );
+      } else {
+        $pageTop.css("bottom", "20px");
+      }
+    });
+
+    $pageTop.click(function () {
+      $("body, html").animate({ scrollTop: 0 }, 500);
+    });
+  };
+
+  const init = function () {
+    scrollTop();
+  };
+
+  init();
 });
