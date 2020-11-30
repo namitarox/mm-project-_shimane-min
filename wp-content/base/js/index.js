ed1a5edcc5241b818a1884d87b052c19a747691d
daichi_sato <daichiexperience@gmail.com>
2020-11-30 23:55:54 +0900

    [Fix] refs #1
    footer navからスムーススクロールでリンクへ飛べる機能の実装

--- wp-content/base/js/index.js
+++ wp-content/base/js/index.js
@@ -31,8 +31,21 @@ $(function () {
     });
   };
 
+  const smoothScroll = function () {
+    $('a[href^="#"]').click(function () {
+      const speed = 500;
+      const headerHeight = $(".l-header").height();
+      const href = $(this).attr("href");
+      const target = $(href == "#" || href == "" ? "html" : href);
+      const position = target.offset().top - headerHeight;
+      $("html, body").animate({ scrollTop: position }, speed, "swing");
+      return false;
+    });
+  };
+
   const init = function () {
     scrollTop();
+    smoothScroll();
   };
 
   init();
