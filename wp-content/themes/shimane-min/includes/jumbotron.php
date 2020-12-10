69c3e226c3e8b406acb738a145c91c9f2ff2676f
daichi_sato <daichiexperience@gmail.com>
2020-12-11 01:21:42 +0900

    [Fix] jumbotronのサムネイル画像がなかったらno-imageの画像を表示させるように修正

--- wp-content/themes/shimane-min/includes/jumbotron.php
+++ wp-content/themes/shimane-min/includes/jumbotron.php
@@ -1,7 +1,13 @@
 <div class="p-news__top-item">
   <a href="<?php the_permalink();?>" class="p-news__top-inner">
     <div class="p-news__top-img">
-      <img src="<?php echo wp_get_attachment_url( get_post_thumbnail_id() );?>" alt="">
+      <?php
+        if(has_post_thumbnail()):
+          the_post_thumbnail();
+        else:
+      ?>
+      <img src="<?php echo get_template_directory_uri();?>/assets/images/common/news-thumbnail.png" alt="詳しくは記事をご覧ください" />
+      <?php endif; ?>
     </div>
     <p class="p-news__top-text">
       <?php limitCharacter($post, 150); ?>
